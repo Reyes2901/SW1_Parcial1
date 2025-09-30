d8eb5b54b36e6cb20a44eadd264063b6e3605bb1
Reyes2901 <reyesponce78@gmail.com>
2025-09-30 10:32:08 -0600

    deploy 10.32

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,20 +1,22 @@
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import diagrams.routing  # Asegúrate que esto exista
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+# Primero establece la configuración de Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
+# Inicializa Django antes de importar cualquier routing que use modelos
 django_asgi_app = get_asgi_application()
-import diagrams.routing  
+
+# Ahora sí importa routing de tus apps
+import diagrams.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,  # Usa la instancia ya creada
     "websocket": AuthMiddlewareStack(
         URLRouter(
             diagrams.routing.websocket_urlpatterns
         )
     ),
 })
-# from django.core.asgi import get_asgi_application
