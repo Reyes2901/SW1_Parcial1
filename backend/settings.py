22b853834baf21288d9e10e4af0d2d04120df4e5
Reyes2901 <reyesponce78@gmail.com>
2025-09-30 05:14:16 -0600

    -diagramador 3.5 y deploy 2.5

--- backend/settings.py
+++ backend/settings.py
@@ -101,19 +101,7 @@ ASGI_APPLICATION = "backend.asgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-""""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        #NAME': 'your_db_name',
-        #'USER': 'your_db_user',
-        #'PASSWORD': 'your_db_password',
-        #'HOST': 'localhost',
-        #'PORT': '5432',
-    }
-}
-"""
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
